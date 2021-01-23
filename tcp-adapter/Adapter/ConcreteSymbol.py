1bd3db2caa390af8b43e809bbe51511311901add
Tiago Ferreira <me@tiferrei.com>
2021-01-23 01:47:37 +0000

    fix

--- tcp-adapter/Adapter/ConcreteSymbol.py
+++ tcp-adapter/Adapter/ConcreteSymbol.py
@@ -39,8 +39,9 @@ class ConcreteSymbol():
             self.isNull = False
             pattern = re.compile("([A-Z+]+)\(([0-9]+),([0-9]+),([0-9]+)\)")
             capture = pattern.match(string)
+            self.flags = capture.group(1)
             if "+" in self.flags:
-                self.flags = "".join(map(lambda x: x[0], capture.group(1).split("+")))
+                self.flags = "".join(map(lambda x: x[0], self.flags.split("+")))
             self.seqNumber = int(capture.group(2))
             self.ackNumber = int(capture.group(3))
             self.payload = "A" * int(capture.group(4))
